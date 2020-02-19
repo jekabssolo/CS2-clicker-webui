4ad01a81a413ba602408986c30c8fb93785db5fb
jekabssoloRBS <jekabs.solovjovs@rbs.lv>
2020-02-19 22:50:16 +0200

    Finished lab activity 5

--- public/game.js
+++ public/game.js
@@ -1,6 +1,6 @@
 
 // Change "test" to any username you'd like to start a new game
-var username = "test";
+var username = "jekabssolo";
 
 var socket = io.connect("https://tictactoe.info/", {transports: ['websocket']});
 setupSocket();
@@ -11,7 +11,11 @@ function setupSocket() {
     // This function is called whenever a new game state is received from the server
     socket.on('gameState', function (jsonGameState) {
         console.log(jsonGameState);
-
+        var gameState = JSON.parse(jsonGameState)
+        document.getElementById("gold").innerHTML = "Gold: " + Math.floor(gameState["gold"])
+        document.getElementById("shovel").innerHTML = "Buy Shovel <br> Cost: " + Math.ceil(gameState["equipment"]["shovel"]["cost"]) + " Current: " + gameState["equipment"]["shovel"]["numberOwned"]
+        document.getElementById("excavator").innerHTML = "Buy Excavator <br> Cost: " + Math.ceil(gameState["equipment"]["excavator"]["cost"]) + " Current: " + gameState["equipment"]["excavator"]["numberOwned"]
+        document.getElementById("mine").innerHTML = "Buy Gold Mine <br> Cost: " + Math.ceil(gameState["equipment"]["mine"]["cost"]) + " Current: " + gameState["equipment"]["mine"]["numberOwned"]
         // TODO: Display the game state on your GUI
         // You must display: current gold, and the name, number owned, and cost for each type of equipment
 
@@ -21,7 +25,11 @@ function setupSocket() {
 
 function initializeGame() {
     socket.emit("register", username);
-
+    document.getElementById("username").innerHTML = "Username: " + username;
+    document.getElementById("gold").innerHTML = "Gold: 0"
+    document.getElementById("shovel").innerHTML = "Buy Shovel <br> Cost: 0 Current: 0"
+    document.getElementById("excavator").innerHTML = "Buy Excavator <br> Cost: 0 Current: 0"
+    document.getElementById("mine").innerHTML = "Buy Gold Mine <br> Cost: 0 Current: 0"
     // TODO: Add any initialization code you'd like to setup your GUI
     // This function is called once when the page loads
 
